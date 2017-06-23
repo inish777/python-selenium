2cd5e056e652c77adcabd86f117a620b2c413875
Andrey Inishev <inish777@gmail.com>
2017-06-23 14:11:13 +0700

    Тест для контактов с разбиением на вспомогательные методы и созданием класса для свойств контакта

---
+++ contact.py
@@ -0,0 +1,22 @@
+class Contact(object):
+    def __init__(self, name, middlename, lastname, nickname):
+        self.name = name
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = ''
+        self.company = ''
+        self.address = ''
+        self.home = ''
+        self.fax = ''
+        self.mobile = ''
+        self.work = ''
+        self.email = ''
+        self.email2 = ''
+        self.email3 = ''
+        self.homepage = ''
+        self.born_year = ''
+        self.anniversiary_year = ''
+        self.address2 = ''
+        self.phone2 = ''
+        self.notes = ''
